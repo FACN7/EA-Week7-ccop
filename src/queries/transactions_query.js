ce3ee3ae911440f9d183493e6e7238f3966915e5
ArielLB <arielelbachar@gmail.com>
2019-10-30 15:04:18 +0200

    the main page and changing queries to promises Relates #25 and #13

--- src/queries/transactions_query.js
+++ src/queries/transactions_query.js
@@ -1,17 +1,13 @@
 const databaseConnection = require("../database/db_connection.js");
 
-const transactions = (email, cb) => {
-  databaseConnection.query(
-    `SELECT u.name,t.balance,t.descrip,t.date FROM users u JOIN transactions t ON u.email=t.email WHERE t.email=$1 ORDER BY t.trans_id desc`,
-    [email],
-    (err, res) => {
-      if (err) {
-        return cb(err);
-      } else {
-        cb(null, res);
-      }
-    }
-  );
+const transactions = (email) => {
+  return new Promise((res)=>{
+    res(databaseConnection.query(`SELECT t.date,t.descrip,t.balance FROM users u JOIN transactions t ON u.email=t.email WHERE t.email=$1 ORDER BY t.trans_id desc`,
+    [email]));
+  })
+
 };
+
+// transactions("ebraheemabbas51@gmail.com6").then(data => console.log(data.rows)).catch(err => console.log(err.message));
 
 module.exports = transactions;
